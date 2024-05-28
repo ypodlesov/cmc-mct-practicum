c9b24be110405cddde1cb8c5e73bb8ee32810ae9
ypodlesov <ypodlesov@gmail.com>
2024-05-28 06:35:07 +0000

    Add graph.

--- task_1_solve_eq/solve_eq.cpp
+++ task_1_solve_eq/solve_eq.cpp
@@ -8,12 +8,18 @@ void Solve(const int64_t n, const std::vector<double>& right_part, std::vector<d
     std::vector<double> betta(n, 0.0);
     std::vector<double> y(n);
     
-    for(int i = 1; i < n - 1; ++i) {
+    y[1] = B;
+    alpha[1] = MC / y[1];
+    betta[1] = right_part[1] / y[1];
+
+    for(int i = 2; i < n - 1; ++i) {
         y[i] = B + A * alpha[i - 1];
         alpha[i] = MC / y[i];
         betta[i] = (right_part[i] - A * betta[i - 1]) / y[i];
     }
 
+    y[n - 1] = B + A * alpha[n - 2];
+    betta[n - 1] = (right_part[n - 1] - A * betta[n - 2]) / y[n - 1];
     x[n - 1] = betta[n - 1];
 
     for(int i = n - 2; i >= 0; --i) {
--- task_1_solve_eq/test.cpp
+++ task_1_solve_eq/test.cpp
@@ -9,8 +9,8 @@
 #include <catch2/generators/catch_generators_all.hpp>
 
 constexpr double I = 1.0;
-constexpr double EPS0 = 1;
-constexpr double EPS2 = 1e-2;
+// constexpr double EPS0 = 1;
+// constexpr double EPS2 = 1e-2;
 
 template <class T1, class T2>
 bool RoughEq(T1 lhs, T2 rhs, T2 epsilon) { 
@@ -29,9 +29,9 @@ bool RoughtLTE(T1 lhs, T2 rhs, T2 epsilon) {
 
 void GeneratRightPart(int64_t n, std::vector<double>& right_part) {
     assert(right_part.size() == static_cast<size_t>(n));
-    const double step = I / (n * n);
+    const double step = I / n;
     for (int i = 1; i < n; ++i) {
-        right_part[i] = 2 * ((i * step + I) * (i * step + I) - 4 * i * i * step * (i * step + I)) / ((i * step + I) * (i * step + I) * (i * step + I) * (i * step + I));
+        right_part[i] = 2 / ((step * i + 1) * (step * i + 1) * (step * i + 1));
     }
 }
 
@@ -41,28 +41,44 @@ static void Test(const int64_t n) {
     GeneratRightPart(n, right_part);
     Solve(n, right_part, x);
     double val, l2_norm = 0, c_norm = 0;
-    for(int64_t i = 1; i < n; i++) {
-        val = std::abs(x[i] - I / (i + 1));
+    const double h = 1.0 / n;
+    for(int64_t i = 1; i < n; ++i) {
+        val = std::abs(x[i] - I / (i * h + I));
         l2_norm += val * val;
-        c_norm = std::max(c_norm, val);
+        c_norm = std::max(c_norm, val * h);
     }
-    l2_norm = std::sqrt(l2_norm) / n;
-    REQUIRE(RoughEq<double, double>(l2_norm, 0, EPS2));
-    REQUIRE(RoughEq<double, double>(c_norm, 0, EPS0));
+    l2_norm = std::sqrt(l2_norm * h * h) / n;
+    std::cout << l2_norm << "\n" << c_norm << std::endl;
 }
 
-TEST_CASE("Size 100") {
-    Test(100);
+TEST_CASE("Size 2") {
+    Test(2);
 }
 
-TEST_CASE("Size 1000") {
-    Test(1000);
+TEST_CASE("Size 4") {
+    Test(4);
 }
 
-TEST_CASE("Size 10000") {
-    Test(10000);
+TEST_CASE("Size 8") {
+    Test(8);
 }
 
-TEST_CASE("Size 100000") {
-    Test(100000);
+TEST_CASE("Size 16") {
+    Test(16);
+}
+
+TEST_CASE("Size 32") {
+    Test(32);
+}
+
+TEST_CASE("Size 64") {
+    Test(64);
+}
+
+TEST_CASE("Size 128") {
+    Test(128);
+}
+
+TEST_CASE("Size 256") {
+    Test(256);
 }
